8933957e778a8d97bd51f180ff0511ff1be59a60
Yuzunyannn <yuzunyannn@outlook.com>
2020-04-29 16:03:17 +0800

    测试内容

--- easyHttp/src/yuzu/easyhttp/Main.java
+++ easyHttp/src/yuzu/easyhttp/Main.java
@@ -4,25 +4,18 @@ import java.io.File;
 
 import yuzu.easyhttp.content.ContentFile;
 import yuzu.easyhttp.controller.ControllerResources;
-import yuzu.easyhttp.controller.ControllerWebsocket;
 
 public class Main {
 
 	public static void main(String[] arg) throws Exception {
 		HttpServer server = new HttpServer(5656);
-		String path = "F:/Project/cdtcg/cdtcg-web/src/main/webapp/WEB-INF/view/";
-
+		String path = "./web/";
 		server.register("/home", (req, rep) -> {
 			return new ContentFile(new File(path + "home.html"));
 		});
-		
-		server.register("/client", (req, rep) -> {
-			return new ContentFile(new File(path + "client.html"));
-		});
-		server.register("/WebTest/clients", new ControllerWebsocket());
+		// server.register("/WebTest/clients", new ControllerWebsocket());
 		server.register("/js/*", new ControllerResources(path + "js"));
 		server.register("/css/*", new ControllerResources(path + "css"));
-
-		server.register("/nice/*", new ControllerResources("D:/Resource/图片"));
+		server.register("/img/*.png", new ControllerResources(path + "img"));
 	}
 }
